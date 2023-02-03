b1e7391dbf9ada0ff262f2cba927274bd4a84569
AC2K <yokouchiko@gmail.com>
2023-02-03 12:30:55 +0900

    master

--- Main/main.cpp
+++ Main/main.cpp
@@ -18,32 +18,5 @@ const int dx[4] = { 1,0,-1,0 };
 const int dy[4] = { 0,1,0,-1 };
 template<class T>inline void chmax(T&x,T y){if(x<y)x=y;}
 template<class T>inline void chmin(T&x,T y){if(x>y)x=y;}
-
-#pragma GCC target("avx2")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    string x;
-    int n;
-    cin>>x>>n;
-    constexpr int alphabet_num='z'-'a'+1;
-    vector<int> idx(alphabet_num);
-    rep(i,x.size()){
-        idx[x[i]-'a']=i;
-    }
-    vector<string> s(n);
-    for(auto&ss:s){
-        cin>>ss;
-    }
-    sort(all(s),[&](string s1,string s2){
-        rep(i,min(s1.size(),s2.size())){
-            if(s1[i]!=s2[i])return idx[s1[i]-'a']<idx[s2[i]-'a'];
-        }
-        return s1.size()<s2.size();
-    });
-    for(auto&ss:s){
-        cout<<ss<<'\n';
-    }
 }
