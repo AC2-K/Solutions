92709812660d3f7398f9fac6eb088376e4a7f43b
AC2K <yokouchiko@gmail.com>
2023-02-24 10:34:15 +0900

    master

--- JOI/sub.cpp
+++ JOI/sub.cpp
@@ -19,4 +19,58 @@ const int dy[4] = { 0,1,0,-1 };
 template<class T>inline void chmax(T&x,T y){if(x<y)x=y;}
 template<class T>inline void chmin(T&x,T y){if(x>y)x=y;}
 int main() {
+    int n;
+    string s;
+    cin>>n>>s;
+    vector<int> sum_j(n+1),sum_o(n+1),sum_i(n+1);
+    rep(i,n){
+        sum_j[i+1]=sum_j[i];
+        sum_o[i+1]=sum_o[i];
+        sum_i[i+1]=sum_i[i];
+
+        if(s[i]=='J')sum_j[i+1]++;
+        else if(s[i]=='O')sum_o[i+1]++;
+        else sum_i[i+1]++;
+    }
+    ll mx = -1;
+
+    //'O'を挿入する
+    rep(i,n){
+        int cnt_j=sum_j[i+1];
+        int cnt_i=sum_i[n]-sum_i[i+1];
+
+        chmax(mx,1ll*cnt_j*cnt_i);
+    }
+    //'J' をpush frontする
+    
+    ll cnt=0;
+    rep(i,n){   //'o'
+    
+        if(s[i]=='O'){
+            cnt+=sum_i[n]-sum_i[i+1];
+        }
+    }
+    
+    chmax(mx,cnt);
+    //'I'をpush backする
+    cnt=0;
+    rep(i,n){   //'o'
+        if(s[i]=='O'){
+            cnt+=sum_j[i];
+        }
+    }
+    chmax(mx,cnt);
+
+
+    //元の個数
+    vector<vector<ll>> dp(n+1,vector<ll>(4));
+    dp[0][0]=1;
+    const string joi="JOI";
+    rep(i,n)rep(j,4){
+        dp[i+1][j]+=dp[i][j];
+        if(j<3&&s[i]==joi[j]){
+            dp[i+1][j+1]+=dp[i][j];
+        }
+    }
+    cout<<dp[n][3]+mx<<'\n';
 }
